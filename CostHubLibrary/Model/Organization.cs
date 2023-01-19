79bb685c7278ee19b73fff22a724b0448d1fa52d
Jacob Kofod <jacob_kofod@yahoo.com>
2023-01-20 00:02:14 +0100

    Added Organization class Added method to get accountable person for owner.

---
+++ CostHubLibrary/Model/Organization.cs
@@ -0,0 +1,20 @@
+﻿using CostHubLibrary.Stores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CostHubLibrary.Model
+{
+    public class Organization
+    {
+        public static Person GetAccountablePerson(string Owner, DateTime PointInTime)
+        {
+            OwnerStore os = new OwnerStore();
+            Person p = os.GetPersonWithRole("Accountable", Owner, PointInTime);
+
+            return p;
+        }
+    }
+}
--- CostHubLibrary/Stores/OwnerStore.cs
+++ CostHubLibrary/Stores/OwnerStore.cs
@@ -26,6 +26,35 @@ namespace CostHubLibrary.Stores
             return list.ToArray();
         }
 
+        public Person GetPersonWithRole(string RoleName, string OwnerName, DateTime PointInTime)
+        {
+            Person person = null;
+
+            using (var connection = new MySqlConnection(this._connectionString))
+            {
+                connection.Open();
+
+                var param = new
+                {
+                    RoleName = RoleName,
+                    OwnerName = OwnerName,
+                    PointInTime = PointInTime
+                };
+
+                person = connection.Query<Person>(
+                    @"SELECT Person.PersonId AS PersonId, Person.Firstname AS Firstname, Person.Lastname AS Lastname, Person.Email AS Email 
+                      FROM Owner,Person,Role 
+                      WHERE Role.Type = @RoleName
+                      AND Owner.Name = @OwnerName
+                      AND Role.OwnerId = Owner.OwnerId
+                      AND Role.PersonId = Person.PersonId
+                      AND Role.StartDateTime < @PointInTime
+                      AND Role.EndDateTime IS NULL", param).FirstOrDefault();
+            }
+
+            return person;
+        }
+
         public Owner Get(string OwnerId)
         {
             Owner item = null;
